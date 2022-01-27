f294e6adefc71d9a8083b972880c37a78954042e
Juan Herrera <juannicolash01@gmail.com>
2022-01-26 23:25:38 -0300

    Fix de librerias

--- src/navigation/NavigationController.tsx
+++ src/navigation/NavigationController.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './../screens/HomeScreen';
 import DetailScreen from './../screens/DetailScreen';
 import {Movie} from '../interfaces/MovieInterface';
@@ -9,7 +9,7 @@ export type RootStackParams = {
   DetailScreen: Movie,
 };
 
-const Stack = createStackNavigator<RootStackParams>();
+const Stack = createNativeStackNavigator<RootStackParams>();
 
 const NavigationController = () => {
   return (
